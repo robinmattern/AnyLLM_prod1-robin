a2a2e994ebb40465c5108b0c3911e2730c9a51ac
Robin Mattern <robin.mattern@gmail.com>
2024-11-11 10:25:53 -0500

    .(41110.03_Update stuff in set-anyllm.sh

--- set-anyllm.sh
+++ set-anyllm.sh
@@ -8,8 +8,9 @@
 ##FD   set-anyllm.sh            |   5356| 10/22/24 08:10|      | v1.04`41022.0810
 ##FD   set-anyllm.sh            |   6240| 10/22/24 09:15|      | v1.04`41022.0915
 ##FD   set-anyllm.sh            |   6391| 10/23/24 07:25|      | v1.05`41023.0725
-##FD   set-anyllm.sh            |   6997| 11/09/24 15:40|      | v1.05`41025.1540
-##FD                            | 
+##FD   set-anyllm.sh            |   6401| 11/09/24 15:40|      | v1.05`41109.1540
+##FD   set-anyllm.sh            |   6508| 11/11/24 10:22|   157| v1.05`41111.1022
+##FD                            |
 ##DESC     .--------------------+-------+---------------+------+-----------------+
 #            This script saves anyllm command to ._0/bin.
 #
@@ -23,14 +24,15 @@
 #            mkScript           |
 #            cpyToBin           |
 #            Sudo               |
-#                               | 
+#                               |
 ##CHGS     .--------------------+----------------------------------------------+
-# .(41016.01 10/16/24 RAM  9:24a| 
-# .(41021.01 10/21/24 RAM 10:06a| 
-# .(41022.01 10/22/24 RAM  8:10a| 
-# .(41022.01 10/22/24 RAM  9:15a| 
-# .(41023.01 10/23/24 RAM  7:25a| 
+# .(41016.01 10/16/24 RAM  9:24a|
+# .(41021.01 10/21/24 RAM 10:06a|
+# .(41022.01 10/22/24 RAM  8:10a|
+# .(41022.01 10/22/24 RAM  9:15a|
+# .(41023.01 10/23/24 RAM  7:25a|
 # .(41109.06 11/09/24 RAM  3:40p| Add this heading and remove some stuff
+# .(41111.03 11/11/24 RAM 10:22a| Add Show command and fix some stuff
 #
 ##PRGM     +====================+===============================================+
 ##ID 69.600. Main0              |
@@ -49,8 +51,10 @@
 function help() {
   echo "  Run . ./set-anyllm.sh commands  (${aVer} OS: ${aOS})"
   echo "    help  This help"
-  echo "    doit  Make Folders"
-  echo "    wipe  Wipe all the setup"
+  echo "    doit  Create command anyllm"           										# .(41111.03.1)
+# echo "    doit  Make folders"                    										##.(41111.03.2)
+  echo "    show  Show commands and ${aBashrc} and \$PATH"    							# .(41111.03.3)
+# echo "    wipe  Wipe all the setup"              										##.(41111.03.4)
   }
 # -----------------------------------------------------------
 
@@ -64,10 +68,10 @@ function setOSvars() {
      aBashrc="$HOME/.bashrc"
      aBinDir="/Home/._0/bin"
      aOS="linux"
-  if [[ "${OS:0:7}" == "Windows" ]]; then 
-     aOS="windows"; 
+  if [[ "${OS:0:7}" == "Windows" ]]; then
+     aOS="windows";
      aBinDir="/C/Home/._0/bin"
-     fi 
+     fi
   if [[ "${OSTYPE:0:6}" == "darwin" ]]; then
      aBashrc="$HOME/.zshrc"
      aBinDir="/Users/Shared/._0/bin"
@@ -92,15 +96,15 @@ function Sudo() {                                                               
 function showEm() {
 
   echo "  aBinDir: '${aBinDir}'"
-  if [ -d "${aBinDir}" ]; then ls -l "${aBinDir}" | awk 'NR > 1 { print "    " $0 }'; fi 
+  if [ -d "${aBinDir}" ]; then ls -l "${aBinDir}" | awk 'NR > 1 { print "    " $0 }'; fi
   echo ""
 
   echo "  .Bashrc: '${aBashrc}'"
-  if [ -f "${aBashrc}" ]; then cat  "${aBashrc}" | awk '{ print "    " $0 }'; fi 
+  if [ -f "${aBashrc}" ]; then cat  "${aBashrc}" | awk '{ print "    " $0 }'; fi
   echo -e "    -------\n"
 
   echo "  PATH:"
-  echo "${PATH}" | awk '{ gsub( /:/, "\n" );  print }' | awk '{ print "    " $0 }'
+  echo "${PATH}" | awk '{ gsub( /:/, "\n" );  print }' | awk '/bin$/ { print "    " $0 }'   # .(41111.03.4)
   }
 # -----------------------------------------------------------
 
@@ -123,12 +127,12 @@ function cpyToBin() {
 # echo " aJPTs_JDir: ${aJPTs_JDir}";
 # echo " aJPTs_GitR: ${aJPTs_GitR}";
 # echo " alias gitr: ${aJPTs_JDir}/gitr.sh";
-# echo " copying run-anyllm.sh and gitr to: \"${aJPTs_JDir}\""; echo ""  
+# echo " copying run-anyllm.sh and gitr to: \"${aJPTs_JDir}\""; echo ""
 
-  if [ ! -d  "${aJPTs_JDir}" ]; then sudo mkdir -p  "${aJPTs_JDir}";                     echo "  Done: created ${aJPTs_JDir}";
-                                     sudo chmod 777 "${aJPTs_JDir}"; fi
+  if [ ! -d  "${aJPTs_JDir}" ]; then sudo mkdir -p  "${aJPTs_JDir}";                    echo "  Done: created ${aJPTs_JDir}";
+                                     Sudo chmod 777 "${aJPTs_JDir}"; fi                 # .(41111.03.5 RAM was sudo)
 
-  if [   -f  "${aAnyLLMscr}" ]; then mkScript "${aAnyLLMscr}" "${aJPTs_JDir}" "anyllm";  echo "  Done: created ${aJPTs_JDir}/anyllm";
+  if [   -f  "${aAnyLLMscr}" ]; then mkScript "${aAnyLLMscr}" "${aJPTs_JDir}" "anyllm"; echo "  Done: created ${aJPTs_JDir}/anyllm";
                                      Sudo chmod 777 "${aAnyLLMscr}"; fi
   }
 # ---------------------------------------------------------------------------
@@ -141,12 +145,12 @@ function cpyToBin() {
 
   if [[ "${aCmd}" == "help"   ]]; then help; fi
   if [[ "${aCmd}" == "showEm" ]]; then showEm; fi
-  if [[ "${aCmd}" == "doIt"   ]]; then setBashrc; fi
+# if [[ "${aCmd}" == "doIt"   ]]; then setBashrc; fi                                    # .(41111.03.6)
   if [[ "${aCmd}" == "doIt"   ]]; then cpyToBin; fi
 
 # ---------------------------------------------------------------------------
 
-  cd "${aRepo_Dir}"  || 
+  cd "${aRepo_Dir}"  ||
 
   exit_wCR
 
